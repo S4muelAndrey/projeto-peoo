720d9635b9468f0ffc02e63fc9747019d64f177f
S4muelAndrey <161077886+S4muelAndrey@users.noreply.github.com>
2025-03-01 22:19:29 -0300

    estatisticas.py

--- projeto-final-peoo-main/estatisticas.py
+++ projeto-final-peoo-main/estatisticas.py
@@ -2,26 +2,27 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from model.persistencia import Persistencia
 
-def gerar_grafico():
-    persistencia = Persistencia("dados/agendamentos.json")
-    agendamentos = persistencia.ler_dados()
-
-    contagem = {}
-    for agendamento in agendamentos:
-        data = agendamento["data"]
-        contagem[data] = contagem.get(data, 0) + 1
-
-    fig, ax = plt.subplots()
-    ax.bar(contagem.keys(), contagem.values())
-    ax.set_xlabel("Data")
-    ax.set_ylabel("Quantidade de Apresentações")
-    ax.set_title("Quantidade de Apresentações por Dia")
-
-    st.pyplot(fig)
-
-def mostrar_estatisticas():
-    st.title("Estatísticas do Sistema")
-    gerar_grafico()
-
-if __name__ == "__main__":
-    mostrar_estatisticas()
+class estatisticas:
+    def gerar_grafico():
+        persistencia = Persistencia("dados/agendamentos.json")
+        agendamentos = persistencia.ler_dados()
+    
+        contagem = {}
+        for agendamento in agendamentos:
+            data = agendamento["data"]
+            contagem[data] = contagem.get(data, 0) + 1
+    
+        fig, ax = plt.subplots()
+        ax.bar(contagem.keys(), contagem.values())
+        ax.set_xlabel("Data")
+        ax.set_ylabel("Quantidade de Apresentações")
+        ax.set_title("Quantidade de Apresentações por Dia")
+    
+        st.pyplot(fig)
+    
+    def mostrar_estatisticas():
+        st.title("Estatísticas do Sistema")
+        gerar_grafico()
+    
+    if __name__ == "__main__":
+        mostrar_estatisticas()
