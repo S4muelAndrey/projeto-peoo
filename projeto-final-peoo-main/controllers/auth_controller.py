97f864ccedab57cf38c963e90cb93ece2ab35778
S4muelAndrey <161077886+S4muelAndrey@users.noreply.github.com>
2025-03-07 08:50:27 -0300

    auth_controller.py

--- projeto-final-peoo-main/controllers/auth_controller.py
+++ projeto-final-peoo-main/controllers/auth_controller.py
@@ -1,13 +1,17 @@
 from models.auth import Auth
-
 class AuthController:
     @staticmethod
     def login(email, password):
+        # Verifica se o login é do admin
+        if email == "admin@adm.com" and password == "SenhaDoAdmin":
+            return {"name": "Administrador", "email": email, "user_type": "Admin"}
+        
+        # Caso contrário, segue com a autenticação normal (ex: usando a classe Auth)
         user = Auth.authenticate(email, password)
-        if user:
-            return user
-        else:
-            raise ValueError('Email ou senha inválidos.')
+        if user is None:
+            raise ValueError("Email ou senha incorretos.")
+        return user
+
 
     @staticmethod
     def register(name, email, password, user_type):
